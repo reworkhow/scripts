1f6a4d1b9e3f687d2cab00debcee25efded39eab
Hao Cheng <reworkhow@gmail.com>
2017-03-30 22:42:32 -0500

    update legend position

--- plots/plots.jl
+++ plots/plots.jl
@@ -11,7 +11,7 @@ d2=[0.339902,0.47473,0.628285,0.782205,0.882108,0.959679,0.966109,0.971257,0.973
 plot(size=(600,400))
 
 #make plots with defined marker types and colors
-plot!(x1,d1,line=(:dash,1),color=:red,marker=(3,0.8),label="MT-BayesC")
+plot!(x1,d1,line=(:dash,1),color=:red,marker=(3,0.8),label="MT-BayesC",legend = :topleft)#bottomright
 plot!(x1,d2,line=(:dashdot,1),color=:blue,marker=(3,0.8),label="MT-BayesC-R")
 
 #add xaxis,yaxis,title
@@ -20,7 +20,9 @@ yaxis!("prediction accuracy",(0.3,1.1),0.3:0.1:1.1,font(10, "Courier")) #name,yl
 title!("Comparison of MT-BayesC and MT-BayesC-R",titlefont=font(10,"Courier"))
 
 #add annotation
+annotate!([(2.5,d1[3]+0.05,text("*",16,:red,:center))])
 annotate!([(3.5,d1[4]+0.05,text("*",16,:red,:center))])
 annotate!([(4.5,d1[5]+0.05,text("*",16,:red,:center))])
 annotate!([(5.5,d1[6]+0.05,text("*",16,:red,:center))])
 annotate!([(6.5,d1[7]+0.05,text("*",16,:red,:center))])
+annotate!([(7.5,d1[8]+0.05,text("*",16,:red,:center))])
